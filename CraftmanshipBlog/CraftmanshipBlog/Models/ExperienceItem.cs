566f9a16f5a5437479374d032227dccc2bd939ab
vnc4567 <benazet.vincent@gmail.com>
2024-01-21 18:24:38 +0100

    v1.1

--- CraftmanshipBlog/CraftmanshipBlog/Models/ExperienceItem.cs
+++ CraftmanshipBlog/CraftmanshipBlog/Models/ExperienceItem.cs
@@ -2,6 +2,18 @@
 
 public class ExperienceItem
 {
+    public ExperienceItem()
+    {
+    }
+
+    public ExperienceItem(string date, string enterprise, string poste, IDictionary<string, List<string>> items)
+    {
+        Date = date;
+        Enterprise = enterprise;
+        Poste = poste;
+        Items = items;
+    }
+
     public string Enterprise { get; set; }
     public string Date { get; set; }
     public string Poste { get; set; }
