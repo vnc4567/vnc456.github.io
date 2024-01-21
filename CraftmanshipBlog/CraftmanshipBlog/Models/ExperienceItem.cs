d1fd7262f67bc25f1af18bdd5d52e35c39d614da
vnc4567 <benazet.vincent@gmail.com>
2024-01-21 13:38:21 +0100

    v1

---
+++ CraftmanshipBlog/CraftmanshipBlog/Models/ExperienceItem.cs
@@ -0,0 +1,9 @@
+﻿namespace CraftmanshipBlog.Models;
+
+public class ExperienceItem
+{
+    public string Enterprise { get; set; }
+    public string Date { get; set; }
+    public string Poste { get; set; }
+    public IDictionary<string, List<string>> Items { get; set; }
+}
